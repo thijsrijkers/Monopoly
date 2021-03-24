bc6cd52add5def760b344acaaa30c26db1887d00
thijsrijkers <thijsrijkers@live.nl>
2021-03-24 14:56:36 +0100

    +implementation of tiles with cast from buidable

--- Monopoly/Board.Building.cs
+++ Monopoly/Board.Building.cs
@@ -12,33 +12,20 @@ namespace Monopoly
 {
     public partial class Board
     {
-        public static Board Build()
+        public static Board Build(int housing)
         {
             // build the board here
             Board board = new Board();
 
-       
-            //for(int i = 0; i < maxNPCs; i++)
-            //{
-            //    // Randomization for NPC pawn figures
-            //    var rng = new Random();
+            Tile startTile = new StartTile();
+            board.AddTile(startTile);
 
-            //    // Get all types that implement Material
-            //    var materials = Assembly.GetExecutingAssembly()
-            //        .GetTypes()
-            //        .Where(x => 
-            //            x.IsAssignableTo(typeof(Material)))
-            //        .ToArray();
-            //    // Create a material with a randomly selected type
-            //    Material mat = (Material)Activator.CreateInstance(materials[rng.Next(0, materials.Length)]);
-
-            //    PawnShape shape = (PawnShape)rng.Next(0, (int)PawnShape.BramShape + 1); // BramShape is the highest possible value
-
-            //    PawnFigure figure = new PawnFigure(shape, mat);
-            //    board.AddPlayer(new NPCPlayer(startTile, figure, 0));
-            //}
-
-
+            for (int i = 0; i < housing; i++)
+            {
+                var rng = new Random();
+                Buildable tile = new Town(rng.Next(200, 2000));
+                board.AddTile((Tile)tile);
+            }
 
             return board;
         }
--- Monopoly/Program.cs
+++ Monopoly/Program.cs
@@ -58,18 +58,19 @@ namespace Monopoly
             PawnFigure shoeFigure = new PawnFigure(shoe, wood);
 
             ///Creation of board
-            Tile startTile = new StartTile();
-            Board board = Board.Build();
-            board.AddTile(startTile);
+            Board board = Board.Build(15);
 
-            board.AddPlayer(new NPCPlayer(startTile, shipFigure, 1000));
-            board.AddPlayer(new NPCPlayer(startTile, shoeFigure, 1000));
-
+            board.AddPlayer(new NPCPlayer(board.GetTiles()[0], shipFigure, 1000));
+            board.AddPlayer(new NPCPlayer(board.GetTiles()[0], shoeFigure, 1000));
 
             ///Player
             PawnFigure playerFigure = new PawnFigure(bramShape, gold);
             HumanPlayer player = new HumanPlayer(board.GetTiles()[0], playerFigure, 1000);
-            Console.WriteLine(player.GetPosition());
+        
+            for(int i = 0; i < board.GetTiles().Count; i++)
+            {
+                Console.WriteLine(board.GetTiles()[i]);
+            }
         }
     }
 }
