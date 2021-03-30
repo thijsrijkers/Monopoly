ce5b1889c9889463781d78dfbb7e7827ac11b85e
thijsrijkers <thijsrijkers@live.nl>
2021-03-30 09:05:18 +0200

    +- Refactor the throw function to the playerobject for later manipulation

--- Monopoly/Program.cs
+++ Monopoly/Program.cs
@@ -78,9 +78,9 @@ namespace Monopoly
 
             Console.WriteLine(player.GetPosition());
 
-            board.DiceThrow();
-            board.DiceThrow();
-            board.DiceThrow();
+            board.NextTurn();
+            board.NextTurn();
+            board.NextTurn();
 
             Console.WriteLine(player.GetPosition());
 
