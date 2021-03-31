8dd340f2aeacafe75af8359ba255b1401f76f7d9
BramGerrits <43609156+BramGerrits@users.noreply.github.com>
2021-03-31 14:02:37 +0200

    randomly adds houses when buying

--- Monopoly/Command/Commands/BuildCommand.cs
+++ Monopoly/Command/Commands/BuildCommand.cs
@@ -11,19 +11,19 @@ namespace Monopoly.Command.Commands
     {
         public void Execute(Board board, PlayerObject executor, PlayerObject target = null)
         {
-            Buildable tile = (Buildable)executor.GetPosition();
-            int idx = board.GetTiles().FindIndex(x => x.Equals(tile));
+            //Buildable tile = (Buildable)executor.GetPosition();
+            //int idx = board.GetTiles().FindIndex(x => x.Equals(tile));
 
-            board.GetTiles()[idx] = new HouseBuilt(tile);
+            //board.GetTiles()[idx] = new HouseBuilt(tile);
 
-            //Change the position to the new housebuilt tile
-            foreach (PlayerObject player in board.GetPlayers())
-            {
-                if (player.GetPosition().Equals(tile)) 
-                { 
-                    player.SetTile(board.GetTiles()[idx]);
-                }
-            }
+            ////Change the position to the new housebuilt tile
+            //foreach (PlayerObject player in board.GetPlayers())
+            //{
+            //    if (player.GetPosition().Equals(tile)) 
+            //    { 
+            //        player.SetTile(board.GetTiles()[idx]);
+            //    }
+            //}
         }
     }
 }
