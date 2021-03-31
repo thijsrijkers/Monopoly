3ff1debe212e58a52c3aefec7fc4a733eecf7e94
BramGerrits <43609156+BramGerrits@users.noreply.github.com>
2021-03-31 13:35:14 +0200

    add house command

--- Monopoly/Board.cs
+++ Monopoly/Board.cs
@@ -93,6 +93,10 @@ namespace Monopoly
             players = new Queue<PlayerObject>(players.Where(s => s != value));
         }
 
+        public Queue<PlayerObject> GetPlayers() {
+            return players;
+        }
+
         public void NextTurn()
         {
             PlayerObject currentPlayer = players.Dequeue();
